811c644d7f56100cbc422124d31352393d046037
Hasnain <has-c@github.com>
2020-10-05 21:44:05 +1300

    use nassigns in Task 1

--- Compsci367/A3/cs367a3/instrumented_solvers.py
+++ Compsci367/A3/cs367a3/instrumented_solvers.py
@@ -18,48 +18,49 @@ def backtracking_search_instrumented(
     The key 'num_assignments' == the number of times csp.assign is called.
     The key 'num_backtracks' == the number of backtracks performed.
     """
+
     class MaxSteps(Exception):
         """Raise to terminate backtracking."""
         def __init__(self, number_of_assignments, number_of_backtracks):
             self.number_of_assignments = number_of_assignments
             self.number_of_backtracks = number_of_backtracks
 
-    def backtrack(assignment, number_of_assignments,number_of_backtracks):
+    def backtrack(assignment, number_of_assignments, number_of_backtracks):
         #if assignment is complete then return 
         if len(assignment) == len(csp.variables):
             return assignment, number_of_assignments, number_of_backtracks
-        #select unassigned variables
+        #select unassigned variable
         var = select_unassigned_variable(assignment, csp)
-        
         for value in order_domain_values(var, assignment, csp):
             #hit max steps raise exception
             if csp.nassigns == max_steps:
+                number_of_assignments = csp.nassigns
                 raise MaxSteps(number_of_assignments, number_of_backtracks)
             #if value is consistent with constraints
             if 0 == csp.nconflicts(var, value, assignment):
-                #add to assignment 
+                #add to assignment
                 csp.assign(var, value, assignment)
-                number_of_assignments += 1 #increment number of assignments
+                number_of_assignments = csp.nassigns #change number of assignments
                 removals = csp.suppose(var, value)
                 if inference(csp, var, value, assignment, removals):
-                    result,number_of_assignments, number_of_backtracks = backtrack(assignment,number_of_assignments,number_of_backtracks)
+                    result, number_of_assignments, number_of_backtracks = backtrack(assignment, number_of_assignments, number_of_backtracks)
                     if result is not None:
                         return result, number_of_assignments, number_of_backtracks
                 csp.restore(removals)
-        #remove value from assignment thus backtrack
+        #backtrack
         csp.unassign(var, assignment)
         number_of_backtracks += 1
-
         return None, number_of_assignments, number_of_backtracks
 
+    #back tracking search instrumented main
     number_of_assignments = 0
     number_of_backtracks = 0
     try:
-        result, number_of_assignments, number_of_backtracks = backtrack({}, number_of_assignments,number_of_backtracks)
+        result, number_of_assignments, number_of_backtracks = backtrack({}, number_of_assignments, number_of_backtracks)
     except MaxSteps as steps:
-        result = None
         number_of_assignments = steps.number_of_assignments
         number_of_backtracks = steps.number_of_backtracks
+        result = None
     assert result is None or csp.goal_test(result)
 
     return {
@@ -67,6 +68,7 @@ def backtracking_search_instrumented(
         "num_assignments": number_of_assignments,
         "num_backtracks": number_of_backtracks
     }
+
 
 
 def min_conflicts_instrumented(csp, max_steps=100_000):
@@ -109,7 +111,7 @@ def min_conflicts_instrumented(csp, max_steps=100_000):
 
     num_repair_assignments = csp.nassigns - number_of_initial_assignments
     number_of_total_assignments = csp.nassigns
-    
+
     return {
         "assignment": None,
         "num_assignments": number_of_total_assignments,
